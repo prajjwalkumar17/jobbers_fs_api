6258f82beb62f7d896ae0a4ab886d01c1d330dbd
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-23 03:35:58 +0530

    first api for getting all jobs

---
+++ Controller/jobController.js
@@ -0,0 +1,5 @@
+exports.getAllJobs = async (req, res) => {
+  return res.status(200).json({
+    message: 'hey',
+  });
+};
--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+const express = require('express');
+const morgan = require('morgan');
+const jobRouter = require('./Routers/jobRouter.js');
+const app = express();
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+app.use('/api/v1/jobs', jobRouter);
+module.exports = app;
