92a25ff4ff9c291333a9c1f3e99e8a429303967c
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-22 10:38:25 +0530

    buggy 1

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const AppError = require('./Utils/appError');
 const GlobalAppError = require('./Utils//errorController');
+const path = require('path');
 const jobRouter = require('./Routers/jobRouter.js');
 const authRouter = require('./Routers/authRouter.js');
 const userRouter = require('./Routers/userRouter');
 const app = express();
 const morgan = require('morgan');
 app.use(morgan('dev'));
+app.use(
+  '/ProfilePics',
+  express.static(path.join(__dirname, 'Uploads/ProfilePics'))
+);
+app.use('/Resumes', express.static(path.join(__dirname, 'Uploads/Resumes')));
 app.use(express.json({ limit: '10kb' }));
 app.use('/api/v1/jobs', jobRouter);
 app.use('/api/v1/auth', authRouter);
