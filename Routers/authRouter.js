49f1fc0bb687b699860eb5a4782b830babb5f106
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-27 19:17:13 +0530

    current_designation in user model

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../Controller/authController');
 const apiFeatures = require('../Utils/apiFeatures');
 const upload = require('multer')();
-// const upload = multer();
+// // const upload = multer();
 router.use(upload.array());
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
